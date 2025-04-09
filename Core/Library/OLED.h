f32e52f605b49b78b61151369c294fd9c0a6c46b
Alva <1255086244@qq.com>
2025-04-10 02:20:23 +0800

    init

---
+++ Core/Library/OLED.h
@@ -0,0 +1,45 @@
+/*
+ * OLED.h
+ *
+ *  Created on: Mar 9, 2025
+ *      Author: Alva
+ *      @brief: 0.96寸四线OLED屏幕驱动库，依赖I2C.h和font.h
+ */
+
+#ifndef LIBRARY_OLED_H_
+#define LIBRARY_OLED_H_
+
+#include "i2c.h"
+#include "font.h"
+#include <math.h>
+#include <stdarg.h>
+
+void OLED_Init();
+void OLED_DisPlay_On();
+void OLED_DisPlay_Off();
+
+void OLED_NewFrame();
+void OLED_ShowFrame();
+void OLED_SetPixel(uint8_t x, uint8_t y, OLED_ColorMode color);
+
+void OLED_DrawLine(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, OLED_ColorMode color);
+void OLED_DrawRectangle(uint8_t x, uint8_t y, uint8_t w, uint8_t h, OLED_ColorMode color);
+void OLED_DrawFilledRectangle(uint8_t x, uint8_t y, uint8_t w, uint8_t h, OLED_ColorMode color);
+void OLED_DrawTriangle(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, uint8_t x3, uint8_t y3, OLED_ColorMode color);
+void OLED_DrawFilledTriangle(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, uint8_t x3, uint8_t y3, OLED_ColorMode color);
+void OLED_DrawCircle(uint8_t x, uint8_t y, uint8_t r, OLED_ColorMode color);
+void OLED_DrawFilledCircle(uint8_t x, uint8_t y, uint8_t r, OLED_ColorMode color);
+void OLED_DrawEllipse(uint8_t x, uint8_t y, uint8_t a, uint8_t b, OLED_ColorMode color);
+void OLED_DrawImage(uint8_t x, uint8_t y, const Image *img, OLED_ColorMode color);
+
+void OLED_PrintASCIIChar(uint8_t x, uint8_t y, char ch, const ASCIIFont *font, OLED_ColorMode color);
+void OLED_PrintASCIIString(uint8_t x, uint8_t y, char *str, const ASCIIFont *font, OLED_ColorMode color);
+void OLED_PrintString(uint8_t x, uint8_t y, char *str, const Font *font, OLED_ColorMode color);
+
+void OLED_ShowChar(uint8_t Line, uint8_t Column, char Char);
+void OLED_ShowString(uint8_t Line, uint8_t Column, char *String);
+void OLED_ShowNum(uint8_t Line, uint8_t Column, uint32_t Number, uint8_t Length);
+void OLED_ShowSignedNum(uint8_t Line, uint8_t Column, int32_t Number, uint8_t Length);
+void OLED_ShowHexNum(uint8_t Line, uint8_t Column, uint32_t Number, uint8_t Length);
+void OLED_format_Show_string(uint8_t x, uint8_t y,char *format,...);
+#endif /* LIBRARY_OLED_H_ */
